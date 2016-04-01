5ddff939da274f5ee5f1c255f1dde60125257c62
barshociaj <fromgithub@asteri.sk>
2016-04-01 22:14:43 +0100

    Added clearSession option

--- src/android/Cookies.java
+++ src/android/Cookies.java
@@ -2,7 +2,7 @@
  * Copyright 2013 Ernests Karlsons
  * https://github.com/bez4pieci
  * http://www.karlsons.net
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -35,9 +35,9 @@ import android.util.Log;
 import android.webkit.CookieManager;
 
 public class Cookies extends CordovaPlugin {
-	
+
 	private final String TAG = "CookiesPlugin";
-	
+
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("clear".equals(action)) {
@@ -45,13 +45,22 @@ public class Cookies extends CordovaPlugin {
             callbackContext.success();
             return true;
         }
-        return false;  // Returning false results in a "MethodNotFound" error.
+				if ("clearSession".equals(action)) {
+            this.clearSession();
+            callbackContext.success();
+            return true;
+        }
+				return false;  // Returning false results in a "MethodNotFound" error.
     }
-	
+
 	public void clear() {
 		Log.v(TAG, "Clearing cookies...");
         CookieManager.getInstance().removeAllCookie();
     }
-	
+
+	public void clearSession() {
+		Log.v(TAG, "Clearing session cookies...");
+	      CookieManager.getInstance().removeSessionCookie();
+	  }
 
 }
--- www/cookies.js
+++ www/cookies.js
@@ -3,7 +3,7 @@
  * Copyright 2013 Ernests Karlsons
  * https://github.com/bez4pieci
  * http://www.karlsons.net
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,7 +30,7 @@ var exec = require('cordova/exec');
  * @constructor
  */
 function Cookies() {
-    
+
 }
 
 /**
@@ -41,6 +41,10 @@ function Cookies() {
  */
 Cookies.prototype.clear = function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, "Cookies", "clear", []);
+};
+
+Cookies.prototype.clearSession = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "Cookies", "clearSession", []);
 };
 
 module.exports = new Cookies();
